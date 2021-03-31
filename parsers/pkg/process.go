1eb50fa06b6f3fcb69fa9d557d5d7277065dc357
Bryan Knight <bryanjknight@gmail.com>
2021-03-31 13:20:04 -0400

    feat: whole foods market - wip

--- parsers/pkg/process.go
+++ parsers/pkg/process.go
@@ -9,6 +9,9 @@ func Parse(request UnparsedReceiptRequest) (ParsedReceipt, error) {
 	if strings.Contains(request.OriginalUrl, "instacart.com") {
 		return ParseInstcartHtmlReceipt(request.Receipt)
 	}
+	if strings.Contains(request.OriginalUrl, "amazon.com") {
+		return ParseWfmHtmlRecipt(request.Receipt)
+	}
 
 	return ParsedReceipt{}, errors.New("unable to match URL with parser")
 }
