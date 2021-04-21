2f14d19cbc73b1ed817a27dbe9e9839c9f589a8c
Bryan Knight <bryanjknight@gmail.com>
2021-04-21 13:48:04 -0400

    docs: added comments to appease the go linter

--- server/services/users/client.go
+++ server/services/users/client.go
@@ -1,13 +1,16 @@
 package users
 
+// Client is an internal mechanism for accessing the user service
 type Client interface {
 	LookupUserByAuthProvider(authProvider string, authSpecificID string) (*User, error)
 }
 
+// DefaultClient is the default client that uses a user repo for access
 type DefaultClient struct {
 	repo UserRepository
 }
 
+// NewDefaultClient creates a default client. Currently defaults to using postgres user repo
 func NewDefaultClient() *DefaultClient {
 
 	// if we wanted to experiment with different database, here's how we would do the switch
@@ -17,6 +20,7 @@ func NewDefaultClient() *DefaultClient {
 	return &DefaultClient{repo: pgRepo}
 }
 
+// LookupUserByAuthProvider finds (or creates) a user based on the auth provider and auth specific ID for the user
 func (c *DefaultClient) LookupUserByAuthProvider(authProvider string, authSpecificID string) (*User, error) {
 	user, err := c.repo.GetOrCreateUserByAuthProviderID(authProvider, authSpecificID)
 	if err != nil {
