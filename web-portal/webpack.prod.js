972224423a32b8be3e36f718182e63be06f0dcf4
Bryan Knight <bryanjknight@gmail.com>
2021-05-06 14:12:59 -0400

    fix: api was not served over https

--- web-portal/webpack.prod.js
+++ web-portal/webpack.prod.js
@@ -9,7 +9,7 @@ const env = {
   REACT_APP_DOMAIN: 'groceryspend-dev.us.auth0.com',
   REACT_APP_CLIENT_ID: 'tonoXWFW9VLF9FHkzNxiUULKtibDkTuf',
   REACT_APP_AUDIENCE: 'https://bknight.dev.groceryspend.io',
-  API_URL: 'http://api.groceryspend.io'
+  API_URL: 'https://api.groceryspend.io'
 }
 
 // reduce it to a nice object, the same as before (but with the variables from the file)
