1b76a0104ddca1d5b3c7ce437a5ee19b13da0c51
Bryan Knight <bryanjknight@gmail.com>
2021-05-13 15:54:05 -0400

    refactor: remove sqlc

--- server/services/users/repos.go
+++ server/services/users/repos.go
@@ -2,8 +2,8 @@ package users
 
 import (
 	"context"
-	"database/sql"
 
+	"github.com/jmoiron/sqlx"
 	// load the postgres river
 	_ "github.com/lib/pq"
 
@@ -13,33 +13,42 @@ import (
 // UserRepository contains the common access patterns for canonical users
 type UserRepository interface {
 	GetOrCreateUserByAuthProviderID(authProvider string, authSpecificID string) (*User, error)
-	// DisableUser(userUUID string) error
 }
 
 // PostgresUserRepository is an implementation of the user datastore using postgres
 type PostgresUserRepository struct {
-	q *Queries
+	db *sqlx.DB
 }
 
 // NewPostgresUserRepository creates a new PostgresUserRepo
 func NewPostgresUserRepository() *PostgresUserRepository {
-	db, err := sql.Open("postgres", utils.GetOsValue("USERS_POSTGRES_CONN_STR"))
+	db, err := sqlx.Open("postgres", utils.GetOsValue("USERS_POSTGRES_CONN_STR"))
 	if err != nil {
 		panic(err)
 	}
-	retval := PostgresUserRepository{&Queries{db: db}}
+	retval := PostgresUserRepository{db: db}
 
 	return &retval
 }
 
 // GetOrCreateUserByAuthProviderID look up user by auth provider id
 func (r *PostgresUserRepository) GetOrCreateUserByAuthProviderID(authProvider string, authSpecificID string) (*User, error) {
-
+	sql := `
+	   INSERT INTO "user" (
+	     auth_provider_id, auth_specific_id
+	   ) VALUES (
+	     $1, $2
+	   )
+	   ON CONFLICT(auth_provider_id, auth_specific_id)
+	   -- TODO: perf impact of doing this
+	   DO UPDATE SET auth_provider_id=EXCLUDED.auth_provider_id
+	   RETURNING *;
+	`
 	// does a record exist?
-	user, err := r.q.GetorCreateUserByAuthProviderId(context.Background(), GetorCreateUserByAuthProviderIdParams{
-		AuthProviderID: authProvider,
-		AuthSpecificID: authSpecificID,
-	})
+	row := r.db.QueryRowxContext(context.Background(), sql, authProvider, authSpecificID)
+
+	var user User
+	err := row.StructScan(&user)
 	if err != nil {
 		return nil, err
 	}
