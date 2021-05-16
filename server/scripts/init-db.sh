d383126b8df0ea8e1a0c5244c7ed52573107add1
Bryan Knight <bryanjknight@gmail.com>
2021-05-16 06:26:31 -0400

    fix: include migration files in docker file

--- server/scripts/init-db.sh
+++ server/scripts/init-db.sh
@@ -3,11 +3,11 @@ set -e
 
 # create databases
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER users;
+    CREATE USER users PASSWORD 'users-password';
     CREATE DATABASE usersdb;
     GRANT ALL PRIVILEGES ON DATABASE usersdb TO users;
 
-    CREATE USER receipts;
+    CREATE USER receipts PASSWORD 'receipts-password';
     CREATE DATABASE receiptsdb;
     GRANT ALL PRIVILEGES ON DATABASE receiptsdb TO receipts;
 EOSQL
