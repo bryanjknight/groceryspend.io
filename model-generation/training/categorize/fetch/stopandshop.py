751e807d26cd7c75488004b7544997fb1be81f10
Bryan Knight <bryanjknight@gmail.com>
2021-05-17 13:47:51 -0400

    feat: standardized categories

--- model-generation/training/categorize/fetch/stopandshop.py
+++ model-generation/training/categorize/fetch/stopandshop.py
@@ -3,12 +3,38 @@
 import gzip
 import xml.etree.ElementTree as ET
 import re
-from typing import List
+from typing import List, Tuple
 
 import requests
 
 from training.categorize.fetch.export import write_table_as_csv
 
+
+SNS_CAT_TO_CANONICAL_CAT_ID = {
+    "frozen": 1,
+    "deli": 2,
+    "health-beauty": 3,
+    "baking-cooking-needs": 4,
+    "beverages": 5,
+    "rice-grains-pasta-beans": 6,
+    "condiments-sauces": 7,
+    "bread-bakery": 8,
+    "snacks-candy": 9,
+    "pet-store": 10,
+    "breakfast-cereal": 11,
+    "laundry-paper-cleaning": 12,
+    "dairy": 13,
+    "soups-canned-goods": 14,
+    "alcoholic-beverages": 15,
+    "home-office": 16,
+    "produce": 17,
+    "baby-childcare": 18,
+    "seafood": 19,
+    "meat": 20,
+    "meal-kits": 21,
+    "floral-garden": 22,
+    "charitable-contributions": 23,
+}
 
 PRODUCT_SITE_MAP_URL = "https://stopandshop.com/groceries/products-sitemap.xml.gz"
 
@@ -24,7 +50,7 @@ def get_site_map() -> ET.Element:
     return root
 
 
-def process_child(e: ET.Element) -> List[str]:
+def process_child(e: ET.Element) -> Tuple[str, int, str]:
 
     loc = e.find("loc")
 
@@ -33,10 +59,14 @@ def process_child(e: ET.Element) -> List[str]:
 
     loc_split = loc.text.replace("https://stopandshop.com/", "").split("/")
     cat = loc_split[1]
+    canonical_cat_id = SNS_CAT_TO_CANONICAL_CAT_ID[cat] if cat in SNS_CAT_TO_CANONICAL_CAT_ID else None
+    if canonical_cat_id is None:
+        raise ValueError(f"No canonical cat ID for {cat}")
+
     sub_cat = loc_split[2]
     item = loc_split[-1].replace(".html", "").replace("-", " ")
 
-    return [item, cat, sub_cat]
+    return (item, canonical_cat_id, sub_cat)
 
 
 def run(output_path: str):
@@ -44,7 +74,16 @@ def run(output_path: str):
     root = get_site_map()
 
     output = []
+    errors: List[ValueError] = []
     for child in root:
-        output.append(process_child(child))
+        try:
+            output.append(process_child(child))
+        except ValueError as e:
+            errors.append(e)
 
     write_table_as_csv(output, f"{output_path}/sns.csv")
+
+    if len(errors) > 0:
+        for err in errors:
+            print(str(err))
+        raise RuntimeError("Missing mappings, please review logs and update mappings")
--- model-generation/web/__init__.py
+++ model-generation/web/__init__.py
@@ -1,20 +1,21 @@
 import json
 import os
 
-from flask import abort, Flask, request, jsonify, make_response, redirect, Response
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import svm
+from flask import abort, Flask, request, jsonify
 
 import joblib
-import numpy as np
-import pandas as pd
-import sklearn
+import yaml
 
 from training.categorize.train import CategorizeTuple
 
 app = Flask(__name__)
 
 cat_tuple: CategorizeTuple = joblib.load(os.environ['CAT_MODEL'])
+cat_id_to_label = {}
+with open(os.environ['CAT_YAML']) as f:
+    y = yaml.safe_load(f)
+    cat_id_to_label = { i['id']: i['name'] for i in y['categories']}
+
 
 def init():
     pass
@@ -27,9 +28,12 @@ def categorize():
 
     item_features = cat_tuple.tfidf.transform(items)
     predictions = cat_tuple.model.predict(item_features)
-    retval = { items[i]: cat_tuple.id_to_cat[predictions[i]] for i in range(len(predictions)) }
+    retval = { items[i]: cat_id_to_label[cat_tuple.id_to_cat[predictions[i]]] for i in range(len(predictions)) }
     return jsonify(retval)
 
+@app.route("/categories", methods=["GET"])
+def all_categories():
+    return jsonify(cat_id_to_label)
 
 if __name__ == "__main__":
     init()
